246fcf4e8ea15b3cb964f7fa5ac37642fda1a1da
Sam Koske <sjkoske@gmail.com>
2023-12-06 10:30:12 -0600

    Small changes to structure and app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template, url_for,flash, redirect, abort
+
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 # Define routes for three pages
 @app.route('/')
 def index():
-    return 'This is the homepage'
+    return render_template('index.html')
 
 @app.route('/admin')
-def about():
-    return 'This is the admin page'
+def admin():
+    return render_template("admin.html")
+
 
 @app.route('/reservations')
-def contact():
-    return 'This is the reservations page'
+def reservations():
+    return render_template("reservations.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+app.run(host="0.0.0.0")
